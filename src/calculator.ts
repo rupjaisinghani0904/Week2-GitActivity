09cfb150502c0f233cea11a67b342e7993591afd
akpav <pavilon.av@northeastern.edu>
2024-09-13 14:37:46 -0400

    subtract operation

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,5 +1,9 @@
 export class Calculator {
-  public add(x: number, y: number) : number {
+  public add(x: number, y: number): number {
     return x + y;
   }
-};
+
+  public subtract(x: number, y: number): number {
+    return x - y;
+  }
+}
